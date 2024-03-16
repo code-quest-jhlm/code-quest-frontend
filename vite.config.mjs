7071868597ca1d797768acfff2c32566da6e1bfc
franmc01 <francmarinc@gmail.com>
2024-03-16 09:39:42 -0500

    chore: configure typography

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Unfonts from 'unplugin-fonts/vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), Unfonts({
+    google: {
+      families: [
+        {
+          name: 'Poppins',
+          styles: 'wght@200;500;600;700;800'
+        },
+        {
+          name: 'Inter',
+          styles: 'wght@100..900',
+        }
+      ]
+    }
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
