e3bc56f2b8211160289a4d4035b4732baa9d5da5
Jipson <jipson09saad@gmail.com>
2024-03-15 20:40:17 -0500

    chore: change tech stack for more simple project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import styles from "./App.module.css";
+
+export default function App() {
+  return (
+    <main className={styles.main}>
+      Hello React + TypeScript + Vite
+    </main>
+  );
+}
