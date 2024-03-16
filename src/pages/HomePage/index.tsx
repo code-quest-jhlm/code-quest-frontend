97eae8e56b28094c1ed319d5ae65c12d39daa52c
franmc01 <francmarinc@gmail.com>
2024-03-16 16:32:12 -0500

    feat: added modal for create draw

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,25 +1,32 @@
 import { Button, Flex, ScrollArea, Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import DashboardLayout from '@/layouts/DashboardLayout';
 import DrawItem from '@/components/DrawItem';
+import DrawCreator from '@/components/DrawCreator';
 
-const HomePage = () => (
-  <DashboardLayout>
-    <Flex justify="space-between" align="center" mb="sm">
-      <Title order={2}>Tus sorteos </Title>
-      <Button size="lg">¡Crear sorteo!</Button>
-    </Flex>
-    <ScrollArea.Autosize mah={620} maw="1280" mx="auto" offsetScrollbars p="md" className="hello">
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-      <DrawItem />
-    </ScrollArea.Autosize>
-  </DashboardLayout>
-);
+const HomePage = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <DashboardLayout>
+      <Flex justify="space-between" align="center" mb="sm">
+        <Title order={2}>Tus sorteos </Title>
+        <Button size="lg" onClick={open}>¡Crear sorteo!</Button>
+      </Flex>
+      <ScrollArea.Autosize mah={620} maw="1280" mx="auto" offsetScrollbars p="md" className="hello">
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+        <DrawItem />
+      </ScrollArea.Autosize>
+      <DrawCreator opened={opened} onClose={close} centered />
+    </DashboardLayout>
+  );
+};
 
 export default HomePage;
