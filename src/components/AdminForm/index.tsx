e7bcc3f03c7028f2176db782ca7daef08db767f1
franmc01 <francmarinc@gmail.com>
2024-03-16 22:00:42 -0500

    style: responsive for auth pages

--- src/components/AdminForm/index.tsx
+++ src/components/AdminForm/index.tsx
@@ -1,16 +1,21 @@
 import { Button, Group, PasswordInput, TextInput } from '@mantine/core';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const AdminForm = () => (
+const AdminForm = () => {
+  const navigate = useNavigate();
+
+  return (
   <form>
     <TextInput label="Usuario" placeholder="Ingrese su usuario" />
     <PasswordInput label="Contraseña" placeholder="Ingrese su contraseña" />
     <Group justify="center" mt="md">
-      <Button type="submit" size="lg">
+      <Button type="submit" size="lg" onClick={() => navigate('/home')}>
         Log in
       </Button>
     </Group>
   </form>
 );
+};
 
 export default AdminForm;
--- src/components/DrawCreator/index.tsx
+++ src/components/DrawCreator/index.tsx
@@ -8,64 +8,69 @@ import {
   TextInput,
 } from '@mantine/core';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface DrawCreatorProps extends ModalBaseProps {
   centered?: boolean;
 }
 
-const DrawCreator: FC<DrawCreatorProps> = (props) => (
-  <>
-    <Modal {...props} title="Crear sorteo" size="xl">
-      <Flex direction="column" rowGap={12}>
-        <TextInput label="Nombre" placeholder="Ej: Sorteo 10M subscriptores" />
-        <TextInput label="Descripción (Opcional)" placeholder="Ej: Lorem ipsum" />
-      </Flex>
-      <Grid mt={24}>
-        <Grid.Col span={6}>
-          <Select
-            leftSectionPointerEvents="none"
-            leftSection="#"
-            w={240}
-            label="Número de ganadores"
-            placeholder="Ej: 1"
-            data={['1', '2', '3', '4']}
-          />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <TextInput
-            label="Máx. participantes"
-            placeholder="Ej: 1200"
-            leftSectionPointerEvents="none"
-            leftSection="#"
-          />
-        </Grid.Col>
-      </Grid>
+const DrawCreator: FC<DrawCreatorProps> = (props) => {
+  const navigate = useNavigate();
 
-      <Grid>
-        <Grid.Col span={6}>
-          <TextInput label="Premio 1" placeholder="Ej: Subscripción Anual DevTalles" />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <TextInput label="Premio 2" placeholder="Ej: Subscripción Trimestral DevTalles" />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <TextInput label="Premio 3" placeholder="Ej: Subscripción Semestral DevTalles" />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <TextInput label="Premio 4" placeholder="Ej: Teclado Redragon" />
-        </Grid.Col>
-      </Grid>
+  return (
+    <>
+      <Modal {...props} title="Crear sorteo" size="xl">
+        <Flex direction="column" rowGap={12}>
+          <TextInput label="Nombre" placeholder="Ej: Sorteo 10M subscriptores" />
+          <TextInput label="Descripción (Opcional)" placeholder="Ej: Lorem ipsum" />
+        </Flex>
+        <Grid mt={24}>
+          <Grid.Col span={6}>
+            <Select
+              leftSectionPointerEvents="none"
+              leftSection="#"
+              w={240}
+              label="Número de ganadores"
+              placeholder="Ej: 1"
+              data={['1', '2', '3', '4']}
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <TextInput
+              label="Máx. participantes"
+              placeholder="Ej: 1200"
+              leftSectionPointerEvents="none"
+              leftSection="#"
+            />
+          </Grid.Col>
+        </Grid>
 
-      <Flex mt={32} justify="flex-end" columnGap={32}>
-        <Button variant="subtle" size="lg" color="white" onClick={props.onClose}>
-          Cancelar
-        </Button>
-        <Button variant="filled" size="lg">
-          ¡Crear sorteo!
-        </Button>
-      </Flex>
-    </Modal>
-  </>
-);
+        <Grid>
+          <Grid.Col span={6}>
+            <TextInput label="Premio 1" placeholder="Ej: Subscripción Anual DevTalles" />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <TextInput label="Premio 2" placeholder="Ej: Subscripción Trimestral DevTalles" />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <TextInput label="Premio 3" placeholder="Ej: Subscripción Semestral DevTalles" />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <TextInput label="Premio 4" placeholder="Ej: Teclado Redragon" />
+          </Grid.Col>
+        </Grid>
+
+        <Flex mt={32} justify="flex-end" columnGap={32}>
+          <Button variant="subtle" size="lg" color="white" onClick={props.onClose}>
+            Cancelar
+          </Button>
+          <Button variant="filled" size="lg" onClick={() => navigate('/draw')}>
+            ¡Crear sorteo!
+          </Button>
+        </Flex>
+      </Modal>
+    </>
+  );
+};
 
 export default DrawCreator;
