612011d107e68c199c03e20e48283e73e46fc4f4
Jipson <jipson09saad@gmail.com>
2024-03-17 16:27:01 -0500

    fix: change create draw method

--- src/services/AdminService.ts
+++ src/services/AdminService.ts
@@ -3,7 +3,7 @@ import { makeRequest } from '@/api';
 const AdminService = {
   adminLogin: <TResponse, TRequest = any>(data: TRequest) => makeRequest<TResponse, TRequest>('POST', '/login', data),
   adminProfile: <TResponse>() => makeRequest<TResponse>('GET', '/profile'),
-  adminDrawCreate: <TResponse>() => makeRequest<TResponse>('GET', '/v1/draw'),
+  adminDrawCreate: <TResponse>(data: any) => makeRequest<TResponse>('POST', '/v1/draw', data),
 };
 
 export default AdminService;
